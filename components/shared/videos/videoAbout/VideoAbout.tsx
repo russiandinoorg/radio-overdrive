bfcf06c7e85b304fd26fd4850dfe84e7663bd18a
Poleexpr <a.c4erkasova@gmail.com>
2025-02-24 17:09:20 +0300

    refactor: change video

--- components/shared/videos/videoAbout/VideoAbout.tsx
+++ components/shared/videos/videoAbout/VideoAbout.tsx
@@ -2,14 +2,13 @@ import styles from './videoAbout.module.scss';
 
 export const VideoAbout = () => (
   <div className={styles.container}>
-    <iframe
-        allowFullScreen
-        allow="clipboard-write; autoplay"
-        className={styles.video}
-        frameBorder="0"
-        src="https://rutube.ru/play/embed/6c8c288569880aa3f5b30740cc2c12e4"
-        title="about"
-       />
+    <video
+      controls
+      loop
+      muted
+      className={styles.video}
+      poster='./videos/video-overdrive-poster.webp'
+      src='./videos/aboutVideo.mp4'
+    />
   </div>
-
 );
