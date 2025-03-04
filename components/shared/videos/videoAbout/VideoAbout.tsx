250cde71a2c1e353d6247f3c1ab90214eb5fa3bb
Anasteisha-WR <LimitEdition@gmail.com>
2025-03-04 20:39:55 +0300

    Changed video screenserver and click on video

--- components/shared/videos/videoAbout/VideoAbout.tsx
+++ components/shared/videos/videoAbout/VideoAbout.tsx
@@ -1,14 +1,35 @@
-import styles from './videoAbout.module.scss';
+"use client";
+import { useRef, useState } from "react";
+import styles from "./videoAbout.module.scss";
 
-export const VideoAbout = () => (
-  <div className={styles.container}>
-    <video
-      controls
-      loop
-      muted
-      className={styles.video}
-      poster='./videos/video-overdrive-poster.webp'
-      src='./videos/aboutVideo.mp4'
-    />
-  </div>
-);
+export const VideoAbout = () => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [_, setIsPlaying] = useState(false);
+
+  const handlePlay = () => {
+    const video = videoRef.current;
+    if (!video) return;
+
+    if (video.paused) {
+      video.play();
+      setIsPlaying(true);
+    } else {
+      video.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  return (
+    <div className={styles.container} onAuxClick={handlePlay}>
+      <video
+        ref={videoRef}
+        className={styles.video}
+        poster="/images/screenserver_about.png"
+        src="/videos/aboutVideo.mp4"
+        loop
+        muted
+        controls
+      />
+    </div>
+  );
+};
