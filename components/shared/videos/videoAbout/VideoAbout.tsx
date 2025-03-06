10ff7f1c64687e35cdd98d293a45955ca8a42b3e
Anasteisha-WR <LimitEdition@gmail.com>
2025-03-06 22:20:32 +0300

    Deleted use_client from Video

--- components/shared/videos/videoAbout/VideoAbout.tsx
+++ components/shared/videos/videoAbout/VideoAbout.tsx
@@ -1,15 +1,10 @@
-"use client";
-import { useRef, useState } from "react";
 import styles from "./videoAbout.module.scss";
 
 export const VideoAbout = () => {
-  const videoRef = useRef<HTMLVideoElement | null>(null);
-  const [_, setIsPlaying] = useState(false);
 
   return (
     <div className={styles.container}>
       <video
-        ref={videoRef}
         className={styles.video}
         poster="/images/screenserver_about.png"
         src="/videos/aboutVideo.mp4"
