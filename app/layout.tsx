f75d9a080d27ab484b995db08603c8c857ddcca9
Aнастасия <anastasiya_sergeevna_khramova@mail.ru>
2025-03-06 10:53:39 +0300

    added library react-ya-metrika

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import '../styles/globals.scss';
-
 import classnames from 'classnames';
-
 import { cocomat, micraDi } from '@/styles/fonts';
+import { YandexMetrika } from '@/components/YandexMetrika';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html className={classnames(cocomat.variable, micraDi.variable)} lang='ru'>
-    <body>{children}</body>
+    <body>
+      <YandexMetrika/>
+      {children}
+    </body>
   </html>
 );
 
