7b882d025d4954c3bf9a47bc9ab0d69b24d7e5d0
Anasteisha-WR <LimitEdition@gmail.com>
2025-03-06 21:55:25 +0300

    Deleted handle play video

--- components/shared/videos/videoAbout/VideoAbout.tsx
+++ components/shared/videos/videoAbout/VideoAbout.tsx
@@ -6,21 +6,8 @@ export const VideoAbout = () => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const [_, setIsPlaying] = useState(false);
 
-  const handlePlay = () => {
-    const video = videoRef.current;
-    if (!video) return;
-
-    if (video.paused) {
-      video.play();
-      setIsPlaying(true);
-    } else {
-      video.pause();
-      setIsPlaying(false);
-    }
-  };
-
   return (
-    <div className={styles.container} onAuxClick={handlePlay}>
+    <div className={styles.container}>
       <video
         ref={videoRef}
         className={styles.video}
