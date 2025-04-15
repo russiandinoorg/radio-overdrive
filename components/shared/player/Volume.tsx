957223c0619f4689a2cc73c3b78680233c84b114
Aнастасия <anastasiya_sergeevna_khramova@mail.ru>
2025-04-15 15:23:56 +0300

    added volume

--- components/shared/player/Volume.tsx
+++ components/shared/player/Volume.tsx
@@ -1,25 +1,10 @@
-import { useState, useEffect, useRef, type FC } from 'react';
-import gsap from "gsap";
+import { useState, useRef, type FC } from 'react';
 import styles from './player.module.scss';
 import { VolumeProps } from './types';
 
 export const Volume: FC<VolumeProps> = ({ audioRef }) => {
-    const [volume, setVolume] = useState(1);
-    const [isOpen, setIsOpen] = useState(false);
+    const [volume, setVolume] = useState(0.5);
     const volumeRef = useRef<HTMLDivElement>(null);
-    const sliderRef = useRef<HTMLDivElement>(null);
-
-    const toggleVolume = () => {
-        setIsOpen(!isOpen);
-    };
-
-    useEffect(() => {
-        if (isOpen) {
-            gsap.to(sliderRef.current, { opacity: 1, y: 0, duration: 0.3 });
-        } else {
-            gsap.to(sliderRef.current, { opacity: 0, y: 10, duration: 0.2 });
-        }
-    }, [isOpen]);
 
     const volumeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         const newVolume = parseFloat(e.target.value);
@@ -31,24 +16,20 @@ export const Volume: FC<VolumeProps> = ({ audioRef }) => {
 
     return (
         <div className={styles.volume} ref={volumeRef}>
-            <button
-                className={styles.volumeIcon}
-                onClick={toggleVolume}
-                aria-label="Настроить громкость"
-            >
-                🔊
-            </button>
-            <div ref={sliderRef} className={`${styles.slider} ${isOpen ? styles.open : ""}`}>
-                <input
-                    className={styles.range}
-                    type="range"
-                    min="0"
-                    max="1"
-                    step="0.01"
-                    value={volume}
-                    onChange={volumeHandler}
-                />
-            </div>
+            <img src="/images/volume.svg" alt="Громкость" />
+            <input
+                className={styles.range}
+                type="range"
+                min="0"
+                max="1"
+                step="0.01"
+                value={volume}
+                onChange={volumeHandler}
+                style={{
+                    background: `linear-gradient(to right, #9337D4 ${volume * 100}%, #ccc ${volume * 100}%)`
+                  }}
+            />
         </div>
+
     )
 }
